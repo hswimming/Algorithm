ffa7f346b37ad4f4a40c0bcd71c9d9f76ef1cbc2
hswimming <118333770+hswimming@users.noreply.github.com>
2024-09-23 22:22:29 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.10 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> copy = new ArrayList<Integer>();
+		
+		int num = 0;
+		
+		for (int i = 0; i <= n; i++) {
+			if (i >= 1 && i % 2 != 0) {
+				num = i;
+				copy.add(num);
+			}
+		}
+		
+		int[] answer = new int[copy.size()];
+		
+		for (int j = 0; j < copy.size(); j++) {
+			answer[j] = copy.get(j);
+		}
+		
+		return answer;
+    }
+}
