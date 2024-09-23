5c2503a0cfe11133db1b5853a9bf928d4d5dfbda
hswimming <118333770+hswimming@users.noreply.github.com>
2024-09-23 22:23:40 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 4.52 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+	    
+    for (int i = 1; i <= n * 6; i++) {
+        if (i * 6 % n == 0) {
+            answer = i;
+            break;
+        }
+    }
+    
+    return answer;
+}
