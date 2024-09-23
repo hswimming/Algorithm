90b46bf782041974ff6769c614c28a822da5e267
hswimming <118333770+hswimming@users.noreply.github.com>
2024-09-23 22:20:32 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.47 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        List<Integer> total = new ArrayList<Integer>();
+		int num = 0;
+		
+		for(int i = 0; i < numbers.length; i++) {
+			num = numbers[i] * 2;
+			total.add(num);
+		}
+		
+		int[] answer = new int[total.size()];
+		
+		for(int j = 0; j < total.size(); j++) {
+			answer[j] = total.get(j);
+		}
+		
+		return answer;
+    }
+}
