ad80fb806f63dd4274a2769e97b52dbd12fd786c
hswimming <118333770+hswimming@users.noreply.github.com>
2024-09-25 13:48:26 +0900

    [level 0] Title: 분수의 덧셈, Time: 4.97 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,20 +1,20 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
-        int numTotal = 0;
-		int denoTotal = 0;
-		int sameNum = 0;
+        int numTotal = 0; // 분자
+        int denoTotal = 0; // 분모
+        int sameNum = 0; // 약분 할 공통 수
         
         numTotal = (numer1 * denom2) + (numer2 * denom1);
-		denoTotal = denom1 * denom2;
+        denoTotal = denom1 * denom2;
 		
-		for (int i = 1; i <= numTotal && i <= denoTotal; i++) {
-			if (numTotal % i == 0 && denoTotal % i == 0) {
-				sameNum = i;
-			}
-		}
+        for (int i = 1; i <= numTotal && i <= denoTotal; i++) {
+            if (numTotal % i == 0 && denoTotal % i == 0) {
+                sameNum = i; // 나머지가 모두 0으로 나눠떨어지는 수로 약분
+            }
+        }
 		
-		int[] answer = {numTotal / sameNum, denoTotal / sameNum};
+        int[] answer = {numTotal / sameNum, denoTotal / sameNum};
 		
-		return answer;
+        return answer;
     }
 }
