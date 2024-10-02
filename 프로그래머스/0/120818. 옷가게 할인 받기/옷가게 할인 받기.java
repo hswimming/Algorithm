48b91877f28aed60831a0de5f5093cbb2b653b54
hswimming <118333770+hswimming@users.noreply.github.com>
2024-10-03 04:45:02 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -9,7 +9,7 @@ class Solution {
             // 30만 원 이상 사면 10%
             answer = (int) (price - (price * 0.1));
         } else if (100000 <= price && price < 300000) {
-            // 10만 원 이상 사면 0.05%
+            // 10만 원 이상 사면 5%
             answer = (int) (price - (price * 0.05));
         } else {
             // 조건을 만족하지 않음
