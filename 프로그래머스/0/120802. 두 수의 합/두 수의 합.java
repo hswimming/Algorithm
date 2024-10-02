1dac66665136670e60791bfee91e021b198df960
hswimming <118333770+hswimming@users.noreply.github.com>
2024-10-03 04:30:51 +0900

    [level 0] Title: 두 수의 합, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/120802. 두 수의 합/두 수의 합.java
+++ 프로그래머스/0/120802. 두 수의 합/두 수의 합.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(int num1, int num2) {
         int answer = -1;
-	        
-	    return answer += 1 + num1 + num2;
+        return answer += 1 + num1 + num2;
     }
 }
