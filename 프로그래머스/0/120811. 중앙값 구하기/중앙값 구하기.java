7779dfb2c39f81d63975934c386df3b91a6b5ee6
hswimming <118333770+hswimming@users.noreply.github.com>
2024-10-03 04:40:09 +0900

    [level 0] Title: 중앙값 구하기, Time: 1.31 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -3,7 +3,6 @@ import java.util.*;
 class Solution {
     public int solution(int[] array) {
         Arrays.sort(array);
-        
         int answer = array[array.length / 2];
         
         return answer;
