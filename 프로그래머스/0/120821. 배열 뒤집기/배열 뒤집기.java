d83b2c9c2971c4afe05bf0ede758abdcf0f8b6ae
hswimming <118333770+hswimming@users.noreply.github.com>
2024-10-03 04:48:44 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -4,7 +4,7 @@ class Solution {
         
         for (int i = 0; i < num_list.length; i++) {
             // 배열의 길이 - 1 => 마지막 인덱스
-            // 0부터 시작하는 i를 추가로 빼서 뒤에서부터 원소 넣기
+            // 0부터 시작하는 i를 추가로 빼서 인덱스 감소시키기
             answer[i] = num_list[(num_list.length - 1) - i];
         }
         
