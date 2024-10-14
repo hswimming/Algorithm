6964dcdaa66ccfc7266e3acb43094b328e656633
hswimming <118333770+hswimming@users.noreply.github.com>
2024-10-14 19:33:36 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int[] emergency) {
+        // [3, 76, 24] => [3, 1, 2]
+        // 3과 같거나 큰 수의 개수는 3개
+        // 76과 같거나 큰 수의 개수는 1개
+        // 24와 같거나 큰 수의 개수는 2개
+        
+        int[] answer = new int[emergency.length];
+        
+        for (int i = 0; i < emergency.length; i++) {
+            for (int j = 0; j < emergency.length; j++) {
+                // 자기 자신과 같거나 큰 수의 인덱스 + 1
+                if (emergency[i] <= emergency[j]) {
+                    answer[i]++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
