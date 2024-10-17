4260b8b8ac6f15e64946e5a184e2720f78499718
hswimming <118333770+hswimming@users.noreply.github.com>
2024-10-17 15:44:31 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.04 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] dot) {
+        int answer = 0;
+        
+        for (int i = 0; i < dot.length; i++) {
+            if (0 < dot[0] && 0 < dot[1]) {
+                answer = 1; // x 좌표와 y 좌표가 모두 양수
+            } else if (dot[0] < 0 && 0 < dot[1]) {
+                answer = 2; // x 좌표가 음수, y 좌표가 양수
+            } else if (dot[0] < 0 && dot[1] < 0) {
+                answer = 3; // x 좌표와 y 좌표가 모두 음수
+            } else {
+                answer = 4; // x 좌표가 양수, y 좌표가 음수
+            }
+        }
+        
+        return answer;
+    }
+}
