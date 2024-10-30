2f515f4a702b2f71d789d374b4786ef5c92f3e8a
hswimming <118333770+hswimming@users.noreply.github.com>
2024-10-31 03:26:47 +0900

    [level 0] Title: 모음 제거, Time: 0.58 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,8 +1,8 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        String pattern = "[aeiou]"; // 정규표현식으로 확인
-        StringBuilder str = new StringBuilder();
+        String pattern = "[aeiou]"; // 정규 표현식으로 확인
+        StringBuilder str = new StringBuilder(); // 문자열 변경 위해 필요
         
         for (int i = 0; i < my_string.length(); i++) {
             // my_string 문자열을 char로 변환하여 확인
@@ -13,6 +13,6 @@ class Solution {
             }
         }
         
-        return answer = String.valueOf(str);
+        return answer = String.valueOf(str); // 자료형 변경
     }
 }
