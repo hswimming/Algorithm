e95e37a7c132212cabfcc613538f2b638227612c
hswimming <118333770+hswimming@users.noreply.github.com>
2024-11-04 21:46:08 +0900

    [level 0] Title: 옹알이 (1), Time: 4.23 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -5,11 +5,14 @@ class Solution {
         
         for (int i = 0; i < babbling.length; i++) {
             for (int j = 0; j < baby.length; j++) {
+                // replace() => 특정 문자열을 새로운 문자열로 대체
+                // replaceAll() => 정규식에 일치하는 문자열들을 전부 다른 문자열로 대체
                 // 발음 배열과 동일한 문자열이 있으면 공백으로 변경
-                babbling[i] = babbling[i].replaceAll(baby[j], " ");
+                babbling[i] = babbling[i].replace(baby[j], " ");
             }
             
-            // 변경한 공백을 없애고 아무것도 남지 않으면 answer 증가
+            // 변경한 공백을 빈 문자열로 변환 후 아무것도 남지 않으면 answer 증가
+            // 빈 문자열 => 발음 배열과 동일한 문자열
             if (babbling[i].replaceAll(" ", "").equals("")) {
                 answer++;
             }
