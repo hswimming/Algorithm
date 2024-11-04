45afcca5464cb3da912c3a607c37a6b37ff6eb79
hswimming <118333770+hswimming@users.noreply.github.com>
2024-11-04 16:27:52 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 2.01 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(String my_string) {
-        int answer = 0; // 정규 표현식으로 확인
-        String pattern = "[a-zA-Z]"; // 문자열 변경 위해 필요
+        int answer = 0;
+        String pattern = "[a-zA-Z]"; // 정규 표현식으로 확인
         
         for (int i = 0; i < my_string.length(); i++) {
             // my_string 문자열을 char로 변환하여 한 글자씩 확인
