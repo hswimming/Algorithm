d989c83065d1879eb57cccca36e44859b0754315
hswimming <118333770+hswimming@users.noreply.github.com>
2024-11-05 20:25:47 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/README.md
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 직사각형 넓이 구하기 - 120860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120860?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120860) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.05 ms
+메모리: 33.4 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 05일 20:15:36
+2024년 11월 05일 20:25:46
 
 ### 문제 설명
 
--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -9,14 +9,37 @@ function solution(dots) {
     var xMax = dots[0][0];
     var yMax = dots[0][1];
     
-    for (let i = 0; i < dots.length; i++) {
+    // Math.min, Math.max 사용하는 경우
+    for (let i = 1; i < dots.length; i++) {
         xMin = Math.min(xMin, dots[i][0]); // x 최소값
         yMin = Math.min(yMin, dots[i][1]); // y 최소값
         xMax = Math.max(xMax, dots[i][0]); // x 최대값
         yMax = Math.max(yMax, dots[i][1]); // y 최대값
     }
     
+    // Math.min, Math.max 사용하지 않을 경우
+    // else if 사용하면 dots 배열의 모든 좌표를 비교하지 못함
+    /*
+    for (let i = 1; i < dots.length; i++) {
+        if (dots[i][0] < xMin) {
+            xMin = dots[i][0]; // x 최소값
+        }
+        
+        if (dots[i][1] < yMin) {
+            yMin = dots[i][1]; // y 최소값
+        }
+        
+        if (dots[i][0] > xMax) {
+            xMax = dots[i][0]; // x 최대값
+        }
+        
+        if (dots[i][1] > yMax) {
+            yMax = dots[i][1]; // y 최대값
+        }
+    }
+    */
+    
     // Math.abs(n) => 음수 처리, 음수에서 양수로만 변환 가능
     // -1을 곱해서 음수를 양수로 처리하는 방법도 있지만 양수일 경우 음수로 바뀌어버림
-    return answer = Math.abs((xMax - xMin) * (yMax - yMin));;
+    return answer = Math.abs((xMax - xMin) * (yMax - yMin));
 }
