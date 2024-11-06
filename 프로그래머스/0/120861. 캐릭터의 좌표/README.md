94bc4a615b02ed1e90272cc28549c39e73464d15
hswimming <118333770+hswimming@users.noreply.github.com>
2024-11-06 20:33:28 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.05 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/README.md
+++ 프로그래머스/0/120861. 캐릭터의 좌표/README.md
@@ -1,10 +1,10 @@
 # [level 0] 캐릭터의 좌표 - 120861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120861) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120861?language=java) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.16 ms
+메모리: 76.2 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 06일 18:21:32
+2024년 11월 06일 20:33:27
 
 ### 문제 설명
 
--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -2,11 +2,11 @@ class Solution {
     public int[] solution(String[] keyinput, int[] board) {
         // up [0, 1] down [0, -1] left [-1, 0] right [1, 0]
         
-        int[] answer = new int[board.length];
-        
         // 맵의 크기에 따라 이동할 칸이 초과하지 않는지 확인
         int x = board[0] / 2;
         int y = board[1] / 2;
+        
+        int[] answer = new int[board.length];
         
         // 입력한 방향키의 배열 요소와 같은 문자에 따라 좌표 이동
         // down, left 경우 음수가 될 수 있으므로 절대값 반환 후 비교
@@ -14,19 +14,13 @@ class Solution {
             if (keyinput[i].equals("up")) {
                 answer[1]++; // [0, 1]
                 answer[1] = (y < answer[1]) ? answer[1] - 1 : answer[1];
-            }
-            
-            if (keyinput[i].equals("down")) {
+            } else if (keyinput[i].equals("down")) {
                 answer[1]--; // [0, -1]
                 answer[1] = (y < Math.abs(answer[1])) ? answer[1] + 1 : answer[1];
-            }
-            
-            if (keyinput[i].equals("left")) {
+            } else if (keyinput[i].equals("left")) {
                 answer[0]--; // [-1, 0]
                 answer[0] = (x < Math.abs(answer[0])) ? answer[0] + 1 : answer[0];
-            }
-            
-            if (keyinput[i].equals("right")) {
+            } else if (keyinput[i].equals("right")) {
                 answer[0]++; // [1, 0]
                 answer[0] = (x < answer[0]) ? answer[0] - 1 : answer[0];
             }
