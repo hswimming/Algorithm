e5bc49c716c6dcf6443af6871c64b950672a10de
hswimming <118333770+hswimming@users.noreply.github.com>
2024-11-11 17:53:52 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.51 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/README.md
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 삼각형의 완성조건 (2) - 120868 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120868?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120868) 
 
 ### 성능 요약
 
-메모리: 76.7 MB, 시간: 1.43 ms
+메모리: 74.3 MB, 시간: 0.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 11일 17:38:27
+2024년 11월 11일 17:53:51
 
 ### 문제 설명
 
--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -3,16 +3,17 @@ import java.util.*;
 class Solution {
     public int solution(int[] sides) {
         // 가장 긴 변이 sides[1]인 경우 될 수 있는 나머지 한 변의 개수 = sides[0];
-        // 나머지 한 변이 가장 긴 변인 경우 = (sides[1] + 1) < max
-        // return = sides[0] + (sides[1] + 1 < max)
+        // 나머지 한 변이 가장 긴 변인 경우 = (sides[1] + 1) < (sides[0] + sides[1])
+        // return = (sides[1] + 1) < (sides[0] + sides[1]) 만족하는 수의 개수 + sides[0]
         
-        Arrays.sort(sides); // 오름차순 정렬
+        Arrays.sort(sides); // 오름차순 정렬, sides의 길이는 2
         
-        int max = sides[0] + sides[1];
-        int num = max - (sides[1] + 1);
+        int max = sides[0] + sides[1]; // 가장 긴 변의 길이 < 다른 두 변의 길이의 합
+        int num = max - (sides[1] + 1); // 나머지 한 변이 가장 긴 변인 경우될 수 있는 한 변의 개수
         int answer = 0;
         
         if (sides[1] == 2) {
+            // 두 변이 1, 2 인 경우
             answer = 1;
         } else {
             answer = sides[0] + num;
