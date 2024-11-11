0d0ef2155447dff8e9aa33b3c01dbc70f69f237d
hswimming <118333770+hswimming@users.noreply.github.com>
2024-11-11 18:42:01 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/README.md
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 삼각형의 완성조건 (2) - 120868 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120868) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120868?language=javascript) 
 
 ### 성능 요약
 
-메모리: 73.9 MB, 시간: 0.80 ms
+메모리: 33.4 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 11일 18:24:37
+2024년 11월 11일 18:42:00
 
 ### 문제 설명
 
--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -6,18 +6,16 @@ function solution(sides) {
     
     // sides의 길이는 2
     // sides의 요소 중 큰 요소 = max, 나머지 요소 = num
-    for (let i = 0; i < sides.length; i++) {
-        if (sides[0] < sides[1]) {
-            max = sides[1];
-            num = sides[0];
-        } else {
-            max = sides[0];
-            num = sides[1];
-        }
-        
-        // 나머지 한 변이 2일 경우 1 리턴
-        answer = num <= 1 ? 1 : sides[0] + sides[1];
+    if (sides[0] < sides[1]) {
+        max = sides[1];
+        num = sides[0];
+    } else {
+        max = sides[0];
+        num = sides[1];
     }
+
+    // 나머지 한 변이 2일 경우 1 리턴
+    answer = num == 1 ? 1 : sides[0] + sides[1];
     
     // 가장 긴 변의 길이는 다른 두 변의 길이의 합보다 작아야 함
     if (answer != 1) {
