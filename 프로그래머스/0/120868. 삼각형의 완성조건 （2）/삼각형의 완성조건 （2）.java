33be202c4c097c4b83c3b84182ccd4d6b861b051
hswimming <118333770+hswimming@users.noreply.github.com>
2024-11-11 18:24:38 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.80 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -9,11 +9,17 @@ class Solution {
         Arrays.sort(sides); // 오름차순 정렬, sides의 길이는 2
         
         int max = sides[0] + sides[1]; // 가장 긴 변의 길이 < 다른 두 변의 길이의 합
-        int num = max - (sides[1] + 1); // 나머지 한 변이 가장 긴 변인 경우될 수 있는 한 변의 개수
+        int num = max - (sides[1] + 1); // 나머지 한 변이 가장 긴 변인 경우 될 수 있는 한 변의 개수
         int answer = 0;
         
-        if (sides[1] == 2) {
-            // 두 변이 1, 2 인 경우
+        /*
+        1. a가 가장 긴 변의 경우 (c < a) 성립해야 하는 조건은 (a < b + c)
+            즉, (a - b < c < a) c의 가능한 정수 개수는 b - 1개
+        2. c가 가장 긴 변인 경우 (a < c) 성립해야 하는 조건은 (c < a + b)
+            즉, (a < c < a + b) c의 가능한 정수 개수는 b개
+        3. (a = c) 경우 b를 더해도 a = c라는 단 하나의 경우만 가능 => (2 * b - 1)
+        */
+        if (sides[0] == 1) {
             answer = 1;
         } else {
             answer = sides[0] + num;
