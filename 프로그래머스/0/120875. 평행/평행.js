86c4647a49433fc1f5e045008272f0cd9971feb6
hswimming <118333770+hswimming@users.noreply.github.com>
2024-11-19 23:19:30 +0900

    [level 0] Title: 평행, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.js
+++ 프로그래머스/0/120875. 평행/평행.js
@@ -1,8 +1,8 @@
 function solution(dots) {
     // 직선이 평행하다 => 두 직선의 기울기가 같다
-    // 직선의 기울기 => y값의 변화량 / x값의 변화
+    // 직선의 기울기 => y값의 변화량 / x값의 변화량
     // 주어진 네 개의 점을 두 개씩 이었을 때, 두 직선이 평행이 되는 경우
-    // 3가지 경우의 수 => [a-b, c-d],[a-c, b-d],[a-d, b-c]
+    // 3가지 경우의 수 => [a-b, c-d], [a-c, b-d], [a-d, b-c]
     
     var answer = 0;
     
@@ -13,7 +13,7 @@ function solution(dots) {
     var ad = (dots[0][1] - dots[3][1]) / (dots[0][0] - dots[3][0]);
     var bc = (dots[1][1] - dots[2][1]) / (dots[1][0] - dots[2][0]);
     
-    // 같으면 두 직선의 기울기가 같음 => 평행
+    // 값이 같으면 두 직선의 기울기가 같으므로 평행
     if (ab === cd) answer = 1;
     if (ac === bd) answer = 1;
     if (ad === bc) answer = 1;
