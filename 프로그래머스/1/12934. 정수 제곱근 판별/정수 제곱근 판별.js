a9bd1e6f541a9f70cc4db867aaf53bea70feabd1
hswimming <118333770+hswimming@users.noreply.github.com>
2024-11-22 03:03:48 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 21.30 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -4,7 +4,7 @@ function solution(n) {
     for (i = 1; i <= n; i++) {
         if (i * i === n) {
             // 양의 정수의 제곱이면 (i + 1) 제곱 리턴
-            answer = ((i + 1) * (i + 1));
+            answer = (i + 1) * (i + 1);
             break;
         } else {
             answer = -1;
