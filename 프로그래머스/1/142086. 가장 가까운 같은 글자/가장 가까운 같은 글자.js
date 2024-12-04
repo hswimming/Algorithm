709a47e38f78f6d792b1cf34d17e2a36f9ff830c
hswimming <118333770+hswimming@users.noreply.github.com>
2024-12-05 04:01:41 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 29.23 ms, Memory: 37.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,17 @@
+function solution(s) {
+    var answer = [];
+    var str = [];
+    
+    var str = [...s].map((cur, idx) => { // 전개 연산자로 배열처럼 사용
+        if (!str.includes(cur)) {
+            answer.push(-1);
+        } else {
+            // 원본 배열을 현재 문자까지 자르고 뒤에서부터 현재 문자의 인덱스 가져오기
+            // 복사한 배열에서 현재와 동일한 문자의 인덱스 뒤에서부터 가져오기 (중복이 있으므로)
+            answer.push(s.slice(0, idx + 1).lastIndexOf(cur) - str.lastIndexOf(cur));
+        }
+        str.push(cur); // 인덱스 계산을 위해 추가
+    });
+    
+    return answer;
+}
