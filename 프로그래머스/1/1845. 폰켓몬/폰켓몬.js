74b113327c259ee748cf28acfa3ba951bef830b9
hswimming <118333770+hswimming@users.noreply.github.com>
2024-12-13 14:48:56 +0900

    [level 1] Title: 폰켓몬, Time: 1.20 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,6 +1,6 @@
 function solution(nums) {
     var answer = 0;
     
-    // 중복 없으면 최대값 리턴
-    return answer = (nums.length / 2 < [...new Set(nums)].length) ? nums.length / 2 : [...new Set(nums)].length;
+    // 중복 없으면 최대값 리턴 (Set 객체는 중복값 허용 X => Set 객체로 변환 후 객체에 들어있는 데이터 수 리턴)
+    return answer = (nums.length / 2 < new Set(nums).size) ? nums.length / 2 : new Set(nums).size;
 }
