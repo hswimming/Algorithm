9af5c3553deb8107ecc8a14cd4ce3310d0732230
hswimming <118333770+hswimming@users.noreply.github.com>
2024-12-17 04:16:33 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -3,12 +3,10 @@ function solution(strings, n) {
     
     // 반환값 음수 => a가 b보다 앞 / 0 => 순서 유지 / 양수 => b가 a보다 앞
     strings.sort((a, b) => {
-        if (a.slice(n, n + 1) < b.slice(n, n + 1)) return -1;
-        if (a.slice(n, n + 1) === b.slice(n, n + 1)) {
-            // n번째 인덱스 값이 동일하면 사전순으로 정렬
-            return a < b ? -1 : 1;
-        }
-        if (a.slice(n, n + 1) > b.slice(n, n + 1)) return 1;
+        if (a.charAt(n) < b.charAt(n)) return -1;
+        // n번째 인덱스 값이 동일하면 사전순으로 정렬
+        if (a.charAt(n) === b.charAt(n)) return a < b ? -1 : 1;
+        if (a.charAt(n) > b.charAt(n)) return 1;
     });
     
     return answer = strings;
