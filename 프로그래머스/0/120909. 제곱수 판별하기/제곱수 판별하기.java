3a0ed6d0a97b7423e9fe99dbbd39fa07935f936b
SooYoung Hwang <118333770+hswimming@users.noreply.github.com>
2025-09-03 17:39:31 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.05 ms, Memory: 69.9 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,16 +1,8 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int num = (int) Math.sqrt(n);
         
-        // 제곱수라면 계속 나눴을 때 나머지 == 0
-        while (n % num == 0) {
-            n /= num;
-            
-            // 제곱수가 아닐 경우 소수점으로
-            if (n < 0) break;
-        }
-        
-        return answer = (n == 1) ? 1 : 2;
+        // 제곱수 => 나머지 == 0
+        return answer = (n % Math.sqrt(n) == 0) ? 1 : 2;
     }
 }
