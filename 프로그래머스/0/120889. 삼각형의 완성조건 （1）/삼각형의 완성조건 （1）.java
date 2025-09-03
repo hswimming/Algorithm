217a14561887dc86722c7100774344c5bf1c323f
SooYoung Hwang <118333770+hswimming@users.noreply.github.com>
2025-09-03 14:53:52 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.49 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 0;
+        Arrays.sort(sides);
+        
+        return answer = (sides[2] < sides[0] + sides[1]) ? 1 : 2;
+    }
+}
