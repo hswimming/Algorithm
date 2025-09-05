f361ff6c1ec6cb24e2057857ea1815b019b28608
SooYoung Hwang <118333770+hswimming@users.noreply.github.com>
2025-09-05 14:57:52 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/0/120898. 편지/편지.java
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -1,11 +1,6 @@
 class Solution {
     public int solution(String message) {
         int answer = 0;
-        
-        for (int i = 0; i < message.length(); i++) {
-            answer++;
-        }
-        
-        return answer *= 2;
+        return answer = message.length() * 2;
     }
 }
