205a48416c5c434cf5fd70c838cb9e17e62239d7
SooYoung Hwang <118333770+hswimming@users.noreply.github.com>
2025-09-08 15:08:54 +0900

    [level 0] Title: 암호 해독, Time: 14.05 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        
+        for (int i = 1; i * code <= cipher.length(); i++) {
+            char c = cipher.charAt(i * code - 1);
+            answer += c;
+        }
+        
+        return answer;
+    }
+}
